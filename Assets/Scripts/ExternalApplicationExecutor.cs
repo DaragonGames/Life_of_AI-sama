d8b6ce5d7ff25787b593bcc57ede09e788c3a883
Stefan <contact@stefan-gerstner.com>
2025-06-04 20:30:59 +0200

    Added Linux as Plattform in ExternalAppExecuter Script

--- Assets/Scripts/ExternalApplicationExecutor.cs
+++ Assets/Scripts/ExternalApplicationExecutor.cs
@@ -6,19 +6,28 @@ public class ExternalApplicationExecutor
     // File Path is different in Editor and Build, but the files can be in an extra Folder outside of the Project within the build folder
     public static void DebugRun(string input, bool python)
     {
+        string pythonPath, output, exePath;
         input = input.Replace(' ', '_');
-        string output;
+
+        if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            pythonPath = "C:/Program Files/Python312/python.exe";
+            exePath = Application.dataPath + "/Temporary/file.exe";
+        }
+        else
+        {
+            pythonPath = "/usr/bin/python3.12";
+            exePath = Application.dataPath + "/Temporary/file";
+        }
 
         if (python)
         {
-            string pythonFilePath = Application.dataPath + "/Temporary/file.py";
-            string pythonPath = "C:/Program Files/Python312/python.exe";
+            string pythonFilePath = Application.dataPath + "/Temporary/UnityAI.py";
             string arguments = pythonFilePath + " " + input;
             output = Run(pythonPath, arguments);
         }
         else
-        {
-            string exePath = Application.dataPath + "/Temporary/file.exe";
+        {            
             output = Run(exePath, input);
         }
 
